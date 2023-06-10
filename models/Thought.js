6ca0a11f8a5c20ba3c263a302e2a7243dffd5bdf
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-10 15:47:27 -0300

    add default to reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
-const { Schema, model } = require('mongoose')
-const Reaction = require('./Reaction')
+const { Schema, Types, model } = require('mongoose')
+// const Reaction = require('./Reaction')
 
 const thoughtSchema = new Schema(
     {
@@ -30,7 +30,8 @@ const thoughtSchema = new Schema(
 const reactionSchema = new Schema(
     {
         reactionId: {
-            type: Schema.Types.ObjectId
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
         },
         reactionBody: {
             type: String,

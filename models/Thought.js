71bc2888e94182b000983a9e0f9e0e42c968d013
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-10 14:00:31 -0300

    move reaction to  thought

--- models/Thought.js
+++ models/Thought.js
@@ -18,8 +18,40 @@ const thoughtSchema = new Schema(
             required: true
         },
         reaction: [Reaction]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
 );
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlenght: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reaction.length;
+});
+
 const Thought = model('thought', thoughtSchema);
-module.exports = Thought
+module.exports = Thought;
+module.exports = reactionSchema

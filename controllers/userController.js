eca7baed3d6007bd5461adc6d1218efc3801a8a4
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-10 14:53:01 -0300

    did the userRoutes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const { User } = require('../models')
 
 module.exports = {
-    async getUser(req, res) {
+    async getUsers(req, res) {
         try {
             const users = await User.find();
             res.json(users);
@@ -13,6 +13,7 @@ module.exports = {
     async getSingleUser(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
 
             if (!user) {
                 return res.status(404).json({ message: 'No user with that ID' });
@@ -26,12 +27,12 @@ module.exports = {
 
     async createUser(req, res) {
         try {
-          const dbUserData = await User.create(req.body);
-          res.json(dbUserData);
+            const dbUserData = await User.create(req.body);
+            res.json(dbUserData);
         } catch (err) {
-          res.status(500).json(err);
+            res.status(500).json(err);
         }
-      },
+    },
 
     async updateUser(req, res) {
         try {
